93811b0515d2de443877e4d09baae15e0f3ce5b9
vagrant <vagrant@homestead>
2020-09-02 10:45:50 +0000

    管理员可以删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -13,4 +13,9 @@ class UserPolicy
     {
         return $currentUser->id === $user->id;
     }
+
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->isAdmin && $currentUser->id != $user->id;
+    }
 }
